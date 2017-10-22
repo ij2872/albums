ca3170d67e0dd5dd546a2a266080dc0d919cac59
Ivan <ijaimes2872@gmail.com>
2017-10-21 20:07:40 -0500

    Add Axios get added

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
 
     componentWillMount(){
-        
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(res => console.log(res));
     }
 
     render(){
